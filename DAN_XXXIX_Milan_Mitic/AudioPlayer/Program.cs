6faba2a222c19bbf9dc8d8f7864a53d993a878e2
Milan992 <mitic.gsm@gmail.com>
2020-06-25 09:19:47 +0200

    Start menu created

--- DAN_XXXIX_Milan_Mitic/AudioPlayer/Program.cs
+++ DAN_XXXIX_Milan_Mitic/AudioPlayer/Program.cs
@@ -10,6 +10,39 @@ namespace AudioPlayer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\tAUDIO PLAYER\n");
+
+            bool menu = true;
+            while (menu == true)
+            {
+                Console.WriteLine("\n1. Add a song");
+                Console.WriteLine("2. View all songs");
+                Console.WriteLine("3. Audio player");
+                Console.WriteLine("4. Exit");
+
+                string options = Console.ReadLine();
+
+                switch (options)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("\nPlease choose one of the following options.");
+                        break;
+                }
+            }
         }
     }
 }
